1de1be86e5dcc8289781882d050e5b48a53c1daa
root <trivedinidhi39@gmail.com>
2020-11-11 21:42:14 +0000

    added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "app.terraform.io/nidhi-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "nidhi-network"
+  network_name = "nidhi-network-1"
   project_id= var.project
   subnets = [
   {
-    subnet_name   = "nidhi-subnet"
+    subnet_name   = "nidhi-subnet-1"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
